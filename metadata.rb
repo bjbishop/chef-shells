9e5ef0f4e8908f235ba4ccfbccdb6878e16e8d59
Bryan Bishop <bryan.bishop@pearson.com>
2015-04-05 14:33:40 -0500

    fist post

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-shells'
+maintainer       'Bryan Bishop'
+maintainer_email 'bryanjbishop@yahoo.com'
+license          'All rights reserved'
+description      'Creates some default shell magic that I wanted'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,32 @@
+directory "/etc/profile.d" do
+  action :create
+  mode "0755"
+  owner "root"
+  group "admin"
+end
+
+execute "update the global bashrc to include the profile.d directory" do
+  action :run
+  command "echo \"for s in \\$( ls /etc/profile.d/*.sh ); do . $s; done #profile.d written by chef\" >> /etc/bashrc"
+  user "root"
+  group "wheel"
+  not_if "grep 'profile.d written by chef' /etc/bashrc"
+end
+
+
+return unless node['current_user']
+
+directory "#{node['current_user']}/.profile.d" do
+  action :create
+  owner node['current_user']
+  group node['current_user']
+  mode "0700"
+end
+
+execute "update the bashrc for user #{node['current_user']} to include the .profile.d directory" do
+  action :run
+  command "echo \"for s in \\$( ls ~/.profile.d/*.sh ); do . $s; done #profile.d written by chef\" >> ~/.bashrc"
+  owner node['current_user']
+  group node['current_user']
+  not_if "grep 'profile.d written by chef' ~/.bashrc"
+end
